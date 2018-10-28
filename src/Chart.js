8c5a1eb2f5f895f91177dce6e3d9e6593d8c5ef2
Taru Airola <turger@wunder.dog>
2018-10-28 18:38:48 +0200

    Add suggestion to select a word from cloud and add some css

--- src/Chart.js
+++ src/Chart.js
@@ -40,23 +40,31 @@ export default class MyComponent extends React.Component {
     const {selectedWord} = this.props
     const link = `https://finna.fi/Search/Results?limit=0&type=AllFields&filter%5B%5D=~building%3A%221%2FNLF%2Farto%2F%22&lookfor0[]=${selectedWord}`
     return (
-      this.props.selectedWord &&
-        <div className='Chart'>
+      <div className='Chart'>
+        {!this.props.selectedWord &&
           <div className='Chart__header'>
-            <div className='Chart__selectedWord'>{this.props.selectedWord}</div>
-            <a className='Chart__button' href={link} target="_blank" rel="noopener noreferrer">Search Finna for articles</a>
+            Click some word from the cloud to see trending of the subject over time!
           </div>
-          {loading && <div className='spinner'/>}
-          {!loading && data &&
-            <LineChart
-              data={data.map((point) => ({...point, active: point.x === activePoint.x ? true : false}))}
-              viewBoxHeight={125}
-              pathColor='#fff'
-              labelsCountY={10}
-              labelsFormatY={()=>{''}}
-            />
-          }
-        </div>
+        }
+        {this.props.selectedWord &&
+          <div>
+            <div className='Chart__header'>
+              <div className='Chart__selectedWord'>{this.props.selectedWord}</div>
+              <a className='Chart__button' href={link} target="_blank" rel="noopener noreferrer">Search Finna for articles</a>
+            </div>
+            {loading && <div className='spinner'/>}
+            {!loading && data &&
+              <LineChart
+                data={data.map((point) => ({...point, active: point.x === activePoint.x ? true : false}))}
+                viewBoxHeight={125}
+                pathColor='#fff'
+                labelsCountY={10}
+                labelsFormatY={()=>{''}}
+              />
+            }
+          </div>
+        }
+      </div>
     )
   }
 }
